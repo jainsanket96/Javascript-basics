e66564ead6e4ee56a8d4e6222eede3b0ec009e16
Sanket Jain <sanket.jain.sgr@gmail.com>
2020-02-05 19:50:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const colorBtn = document.querySelector('.colorBtn');
-const bodyBcg= document.querySelector('body');
-const colors = ['yellow','red','greeen','#3b5998'];
-colorBtn.addEventListener('click',changeColor);
-function changeColor (){
-    //bodyBcg.style.backgroundColor = colors[2];
-    let random = Math.floor(Math.random()*colors.length)
-    bodyBcg.style.backgroundColor = colors[random];
-}
